bb2ee062e0cd20a4031dcc9150ffa69624dc0860
bilguun70b1123@gmail.com <bilguun70b1123@gmail.com>
2022-09-12 12:20:36 +0800

    Navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import All from "./components/All";
+
+function App() {
+  return (
+    <div>
+      <All />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/All.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+
+const All = (props) => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default All;
